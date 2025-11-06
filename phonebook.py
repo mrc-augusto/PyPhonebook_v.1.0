ffe03372cdcf60c6de0bad20a7b728a2cfadbd2c
Maurício Augusto <mrc_augusto@hotmail.com>
2025-11-06 14:44:54 -0300

    Initial commit
    Add the initial structure for the Phonebook Challenge project with the phonebook.py file.

---
+++ phonebook.py
@@ -0,0 +1,39 @@
+phonebook = []
+
+while True:
+  print('\nMenu da Agenda Telefônica')
+  print('1. Adicionar contato')
+  print('2. Visualizar lista de contatos:')
+  print('3. Editar um contato')
+  print('4. Marcar/Desmarcar contato como favorito')
+  print('5. Visualizar contatos favoritos')
+  print('6. Deletar contato')
+  print('7. Sair da Agenda Telefônica')
+
+  choice = input('Escolha a opção desejada (1-7): ')
+
+  if choice == '1':
+    pass
+
+  elif choice == '2':
+    pass
+
+  elif choice == '3':
+    pass
+
+  elif choice == '4':
+    pass
+
+  elif choice == '5':
+    pass
+
+  elif choice == '6':
+    pass
+
+  elif choice == '7':
+    break
+
+  else:
+    print('Opção inválida. Por favor, escolha uma opção entre 1 e 7.')
+
+print('Obrigado por usar a Agenda Telefônica. Até logo!')
