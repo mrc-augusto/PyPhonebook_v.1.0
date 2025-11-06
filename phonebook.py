02029864d0e6f06bd48b5284726d4f5967e40994
Maurício Augusto <mrc_augusto@hotmail.com>
2025-11-06 15:29:22 -0300

    feat: add screen clearing after each sucessful action

--- phonebook.py
+++ phonebook.py
@@ -1,3 +1,7 @@
+import os
+def add_contact():
+  return
+
 phonebook = []
 
 while True:
@@ -13,21 +17,35 @@ while True:
   choice = input('Escolha a opção desejada (1-7): ')
 
   if choice == '1':
-    pass
+    name = input('Digite o nome do contato:')
+    phone = input('Digite o número de telefone do contato:')
+    email = input('Digite o email do contato:')
+    
+    os.system('cls' if os.name == 'nt' else 'clear')
 
   elif choice == '2':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     pass
 
   elif choice == '3':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     pass
 
   elif choice == '4':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     pass
 
   elif choice == '5':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     pass
 
   elif choice == '6':
+    os.system('cls' if os.name == 'nt' else 'clear')
+
     pass
 
   elif choice == '7':
